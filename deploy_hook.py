d96a715dc0efc44cd13cd41baba7c7bb39528c77
bohan <bohanzhang@foxmail.com>
2015-06-03 16:40:12 +0800

    debug

--- deploy_hook.py
+++ deploy_hook.py
@@ -1,12 +1,17 @@
-from flask import Flask
+# coding=utf-8
+from flask import Flask, request
 
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/', methods=['post'])
 def hello_world():
+    # 表单
+    print request.form
+    print request.args
+    print request.data
     return 'Hello World!'
 
 
 if __name__ == '__main__':
-    app.run()
+    app.run(host='0.0.0.0', debug=True)
